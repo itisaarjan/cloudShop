1626fd743adc00a592b586e7da3f450bc39589d2
itisaarjan <arjansubedi@usf.edu>
2025-05-31 16:01:09 -0700

    Created DB table for productCatalog

--- productCatalogService/cdk/bin/cdk.ts
+++ productCatalogService/cdk/bin/cdk.ts
@@ -4,5 +4,8 @@ import { CdkStack } from '../lib/stack';
 
 const app = new cdk.App();
 new CdkStack(app, 'CdkStack', {
-  
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
 });
--- productCatalogService/cdk/lib/stack.ts
+++ productCatalogService/cdk/lib/stack.ts
@@ -1,10 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamoDB from 'aws-cdk-lib/aws-dynamodb';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     
+    const table = new dynamoDB.Table(this, 'ProductCatalogTable', {
+      tableName: 'ProductCatalog',
+      partitionKey: { name: "productId", type: dynamoDB.AttributeType.STRING },
+      billingMode: dynamoDB.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Only for dev/test environments
+      pointInTimeRecovery: true, // Enable point-in-time recovery
+      timeToLiveAttribute: 'ttl' // TTL attribute for item expiration
+    });
+
+    new cdk.CfnOutput(this, 'TableName', {
+      value: table.tableName,
+      description: 'The name of the DynamoDB table for product catalog'
+    });
   }
 }
