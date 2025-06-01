910487ab0c38b7f74bcb79f8a8296071b1462569
itisaarjan <arjansubedi@usf.edu>
2025-05-31 17:37:04 -0700

    Added lambda function for productCatalog

--- productCatalogService/cdk/lib/stack.ts
+++ productCatalogService/cdk/lib/stack.ts
@@ -1,23 +1,40 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as dynamoDB from 'aws-cdk-lib/aws-dynamodb';
+import * as lambdaNodejs from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as path from 'path';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    
+
+    // DynamoDB Table
     const table = new dynamoDB.Table(this, 'ProductCatalogTable', {
       tableName: 'ProductCatalog',
       partitionKey: { name: "productId", type: dynamoDB.AttributeType.STRING },
       billingMode: dynamoDB.BillingMode.PAY_PER_REQUEST,
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // Only for dev/test environments
-      pointInTimeRecovery: true, // Enable point-in-time recovery
-      timeToLiveAttribute: 'ttl' // TTL attribute for item expiration
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      pointInTimeRecovery: true,
+      timeToLiveAttribute: 'ttl'
     });
 
-    new cdk.CfnOutput(this, 'TableName', {
-      value: table.tableName,
-      description: 'The name of the DynamoDB table for product catalog'
+    // Lambda Function
+    const productLambda = new lambdaNodejs.NodejsFunction(this, 'ProductLambda', {
+      entry: path.join(__dirname, '../../lambda/productHandler.ts'),
+      handler: 'handler',
+      runtime: cdk.aws_lambda.Runtime.NODEJS_18_X,
+      environment: {
+        TABLE_NAME: table.tableName
+      }
+    });
+
+    // Grant Lambda permission to read/write DynamoDB
+    table.grantReadWriteData(productLambda);
+
+    // Optional: Output for verification
+    new cdk.CfnOutput(this, 'LambdaRoleArn', {
+      value: productLambda.role!.roleArn,
+      description: 'The IAM Role ARN for the ProductLambda'
     });
   }
 }
