658ec385fd2a49f4379994f46d528d214b6cfe8c
itisaarjan <arjansubedi@usf.edu>
2025-06-16 19:20:40 -0700

    Adding CICD

---
+++ .github/scripts/CDKbootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+REGION=${AWS_REGION}
+
+
+if [ -z "$REGION" ]; then
+  echo "AWS_REGION is not set. Exiting."
+  exit 1
+fi
+
+echo "Checking if CDKToolkit stack exists in region $REGION..."
+
+if ! aws cloudformation describe-stacks \
+    --stack-name "$STACK_NAME" \
+    --region "$REGION" > /dev/null 2>&1; then
+
+  echo "CDKToolkit not found. Running bootstrap..."
+  ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+  npx cdk bootstrap aws://$ACCOUNT_ID/$REGION
+else
+  echo "CDKToolkit already exists in $REGION. Skipping bootstrap."
+fi
