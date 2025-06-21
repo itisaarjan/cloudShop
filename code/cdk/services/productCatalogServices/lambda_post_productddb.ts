2779100d5862dba2a99e23ece23d6ee40c609d96
itisaarjan <arjansubedi@usf.edu>
2025-06-21 10:50:09 -0700

    adding schema validation

--- code/cdk/services/productCatalogServices/lambda_post_productddb.ts
+++ code/cdk/services/productCatalogServices/lambda_post_productddb.ts
@@ -1,22 +1,11 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { PutItemCommand } from '@aws-sdk/client-dynamodb';
+import { Product, zodProduct } from './lambda_update_productddb';
+import {z} from "zod/v4"
+
 const client= new DynamoDBClient({});
 const tableName=process.env.table_name;
-
-interface Product{
-    id: string;
-    name: string;
-    description: string;
-    price: number;
-    stock: number;
-    category: string;
-    imageUrl: string
-}
-
-function validateAllProps(product: Product): boolean {
-    return product.id && product.name && product.description && product.price?true:false && product.stock?true:false && product.category;
-}
 
 export async function handler(event:APIGatewayProxyEvent):Promise<APIGatewayProxyResult>{
     if (!event.body) {
@@ -25,47 +14,49 @@ export async function handler(event:APIGatewayProxyEvent):Promise<APIGatewayProx
             body: JSON.stringify({ message: "Request body is required" })
         };
     }
-    const product:Product= JSON.parse(event.body);
-
-    if(!validateAllProps(product)){
-        return {
-            statusCode: 400,
-            body: JSON.stringify({message: "All properties are required"})
-        };
-    }
-    const command = new PutItemCommand({
-  TableName: tableName,
-  Item: {
-    id: { S: product.id },
-    name: { S: product.name },
-    description: { S: product.description },
-    price: { N: product.price.toString() },
-    stock: { N: product.stock.toString() }, 
-    category: { S: product.category },
-    imageUrl: {S:product.imageUrl}       
-  }
-});
-
+    
     try {
+        const body = JSON.parse(event.body);
+        const product: Product = zodProduct.parse(body);
+
+        const command = new PutItemCommand({
+            TableName: tableName,
+            Item: {
+                id: { S: product.id },
+                name: { S: product.name },
+                description: { S: product.description },
+                price: { N: product.price.toString() },
+                stock: { N: product.stock.toString() }, 
+                category: { S: product.category },
+                imageUrl: {S:product.imageUrl}       
+            },
+            ConditionExpression: 'attribute_not_exists(id)'
+            });
+        
         const result = await client.send(command);
         return {
             statusCode: 201,
             body: JSON.stringify({
                 message: "Product created successfully",
                 product: product
-            })
-        };
+            })}
     } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: "Error creating product",
-                error: error
-            })
+        if (error instanceof z.ZodError) {
+              return {
+                statusCode: 400,
+                body: JSON.stringify({
+                  message: "Zod Validation failure",
+                  errors: error.issues
+                })
+              };
+            } else {
+              return {
+                statusCode: 500,
+                body: JSON.stringify({
+                  message: "Error on the DB side",
+                  error: String(error)
+                })
+              };
         }
     }
-    
-
-
-
 }
--- code/cdk/services/productCatalogServices/lambda_update_productddb.ts
+++ code/cdk/services/productCatalogServices/lambda_update_productddb.ts
@@ -1,68 +1,93 @@
 import { DynamoDBClient, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { z } from "zod/v4";
 
+const client = new DynamoDBClient({});
+const tableName = process.env.table_name;
 
-const client= new DynamoDBClient({});
-const tableName=process.env.table_name;
-
-
-
-export async function handler (event: APIGatewayProxyEvent):Promise<APIGatewayProxyResult> {
-    if(!event.body){
-        return{
-            statusCode: 400,
-            body: JSON.stringify({ message: "Request body is required" })
-        }
-    }
-
-    const product = JSON.parse(event.body);
-
-    if(!product.id){
-        return{
-            statusCode: 400,
-            body: JSON.stringify({message: "Product ID is required"})
-        }
-    }
-
-    const command = new UpdateItemCommand({
-  TableName: tableName,
-  Key: {
-    id: { S: product.id }
-  },
-  UpdateExpression: "SET #name = :name, #description = :description, #price = :price, #stock = :stock, #category = :category",
-  ExpressionAttributeNames: {
-    "#name": "name",
-    "#description": "description",
-    "#price": "price",
-    "#stock": "stock",
-    "#category": "category"
-  },
-  ExpressionAttributeValues: {
-    ":name": { S: product.name },
-    ":description": { S: product.description },
-    ":price": { N: product.price.toString() },
-    ":stock": { N: product.stock.toString() },
-    ":category": { S: product.category }
-  },
-  ReturnValues: "ALL_NEW"
+export const zodProduct = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  price: z.number().min(0),
+  stock: z.number().min(0),
+  category: z.string(),
+  imageUrl: z.string()
 });
 
-try {
-    const result= await client.send(command);
+export type Product = z.infer<typeof zodProduct>;
+
+export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+  if (!event.body) {
     return {
-        statusCode: 200,
-        body:JSON.stringify({
-            message: "Product updated successfully",
-            product: result.Attributes
+      statusCode: 400,
+      body: JSON.stringify({ message: "Request body is required" })
+    };
+  }
+
+  try {
+    const body = JSON.parse(event.body);
+    const product: Product = zodProduct.parse(body);
+
+    const command = new UpdateItemCommand({
+      TableName: tableName,
+      Key: {
+        id: { S: product.id }
+      },
+      UpdateExpression: `
+        SET #name = :name,
+            #description = :description,
+            #price = :price,
+            #stock = :stock,
+            #category = :category,
+            #imageUrl = :imageUrl
+      `,
+      ExpressionAttributeNames: {
+        "#name": "name",
+        "#description": "description",
+        "#price": "price",
+        "#stock": "stock",
+        "#category": "category",
+        "#imageUrl": "imageUrl"
+      },
+      ExpressionAttributeValues: {
+        ":name": { S: product.name },
+        ":description": { S: product.description },
+        ":price": { N: product.price.toString() },
+        ":stock": { N: product.stock.toString() },
+        ":category": { S: product.category },
+        ":imageUrl": { S: product.imageUrl }
+      },
+      ReturnValues: "ALL_NEW"
+    });
+
+    const result = await client.send(command);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Product updated successfully",
+        product: result.Attributes
+      })
+    };
+
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "Zod Validation failure",
+          errors: error.issues
         })
-    }
-} catch (error) {
-    return {
+      };
+    } else {
+      return {
         statusCode: 500,
         body: JSON.stringify({
-            message: "Error updating product",
-            error: error
-        })  
+          message: "Error on the DB side",
+          error: String(error)
+        })
+      };
     }
-}
+  }
 }
