e849297486eb7df775fe6037867a67e1580f18bd
itisaarjan <arjansubedi@usf.edu>
2025-07-01 19:21:06 -0700

    Adding workflow file

--- code/cdk/bin/cloudShop.ts
+++ code/cdk/bin/cloudShop.ts
@@ -4,12 +4,12 @@ import { ProductCatalogStack } from '../lib/productCatalog/stack';
 import { OIDCStack } from '../lib/CICD/githubOIDC';
 
 const app = new cdk.App();
-new OIDCStack(app,'cicdStack',{
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION
-  }
-})
+// new OIDCStack(app,'cicdStack',{
+//   env: {
+//     account: process.env.CDK_DEFAULT_ACCOUNT,
+//     region: process.env.CDK_DEFAULT_REGION
+//   }
+// })
 new ProductCatalogStack(app, 'ProductCatalog', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
