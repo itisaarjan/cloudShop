3092b578f1be1b9a2e6e46d38063e15e1f310a43
itisaarjan <arjansubedi@usf.edu>
2025-07-01 20:16:07 -0700

    Adding frontend stack

--- code/cdk/bin/cloudShop.ts
+++ code/cdk/bin/cloudShop.ts
@@ -3,13 +3,15 @@ import * as cdk from 'aws-cdk-lib';
 import { ProductCatalogStack } from '../lib/productCatalog/stack';
 import { OIDCStack } from '../lib/CICD/githubOIDC';
 import { PaymentStack } from '../lib/paymentService/stack';
+import { FrontendStack } from '../lib/frontendService/stack';
 
 const app = new cdk.App();
-// new OIDCStack(app,'cicdStack',{
-//   env: {
-//     account: process.env.CDK_DEFAULT_ACCOUNT,
-//     region: process.env.CDK_DEFAULT_REGION
-//   }
-// })
+new OIDCStack(app,'cicdStack',{
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+})
 new ProductCatalogStack(app, 'ProductCatalog');
 new PaymentStack(app, 'PaymentService');
+new FrontendStack(app, "FrontendStack");
---
+++ code/cdk/lib/frontendService/stack.ts
@@ -0,0 +1,65 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as certificateManager from 'aws-cdk-lib/aws-certificatemanager';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
+
+export class FrontendStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const bucket = new s3.Bucket(this, 'CloudShopBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
+
+    const hostedZone = new route53.PublicHostedZone(this, 'HostedZone', {
+      zoneName: 'cloudshop.click',
+    });
+
+    const certificate = new certificateManager.Certificate(this, 'Certificate', {
+      domainName: 'www.cloudshop.click',
+      certificateName: 'CloudShop Certificate',
+      validation: certificateManager.CertificateValidation.fromDns(hostedZone),
+    });
+
+    const distribution = new cloudfront.Distribution(this, 'CloudFrontDistribution', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(bucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      domainNames: ['www.cloudshop.click'],
+      certificate,
+      errorResponses: [
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+          ttl: cdk.Duration.seconds(0),
+        },
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+          ttl: cdk.Duration.seconds(0),
+        },
+      ],
+    });
+
+    new route53.ARecord(this, 'ARecord', {
+      zone: hostedZone,
+      recordName: 'www',
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+    });
+
+    new route53.ARecord(this, 'RootARecord', {
+      zone: hostedZone,
+      recordName: '',
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+    });
+  }
+}
